5fcdcbdeab20053049c535230e8a0b9eb8dd8b8f
Élix Neto <elixneto@gmail.com>
2019-11-14 12:17:57 -0400

    Override equals de Entidade

--- StartOnion.Camada.Dominio/Entidade.cs
+++ StartOnion.Camada.Dominio/Entidade.cs
@@ -13,5 +13,22 @@ namespace StartOnion.Camada.Dominio
         }
 
         public void Validar(IValidator validador) => base.AdicionarNotificacoes(validador.Validate(this).Errors);
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+
+            if (this.GetType() != obj.GetType())
+                return false;
+
+            return Id == ((Entidade)obj).Id;
+        }
+
+        public override int GetHashCode() => Id.GetHashCode();
+
+        public static bool operator ==(Entidade e1, Entidade e2) => Equals(e1, e2);
+
+        public static bool operator !=(Entidade e1, Entidade e2) => !Equals(e1, e2);
     }
 }
