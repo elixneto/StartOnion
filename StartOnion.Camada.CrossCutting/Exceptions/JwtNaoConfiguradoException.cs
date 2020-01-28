d79beba49f4800a035031b9d3d12089f612bdd46
Élix Neto <elixneto@gmail.com>
2020-01-28 19:22:15 -0400

    CorssCutting

--- StartOnion.Camada.CrossCutting/Exceptions/JwtNaoConfiguradoException.cs
+++ StartOnion.Camada.CrossCutting/Exceptions/JwtNaoConfiguradoException.cs
@@ -2,7 +2,7 @@
 
 namespace StartOnion.Camada.CrossCutting.Exceptions
 {
-    public class JwtNaoConfiguradoException : Exception
+    public sealed class JwtNaoConfiguradoException : Exception
     {
         public JwtNaoConfiguradoException() : base("Configure no appsetings.json: \"Jwt\":{ \"SecurityKey\": \"suachave\", \"Issuer\": \"seuissuer\", \"Audience\":\"seuaudience\"}")
         {
---
+++ StartOnion.Camada.CrossCutting/Utilidades/Compactacao/Compactador.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+
+namespace StartOnion.Camada.CrossCutting.Utilidades.Compactacao
+{
+    /// <summary>
+    /// Compress
+    /// </summary>
+    public static class Compactador
+    {
+        /// <summary>
+        /// Compacta 1 arquivo
+        /// </summary>
+        /// <param name="arquivoEmBytes">Conteúdo do arquivo em Bytes</param>
+        /// <param name="caminhoDeSaidaZip"></param>
+        /// <returns></returns>
+        public static byte[] CompactarUm(byte[] arquivoEmBytes, string caminhoDeSaidaZip)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create))
+                    using (var streamDeEscrita = new BinaryWriter(zip.CreateEntry(caminhoDeSaidaZip).Open()))
+                        streamDeEscrita.Write(arquivoEmBytes);
+
+                return memoryStream.ToArray();
+            }
+        }
+        
+        // TODO: testar esse método
+        public static byte[] CompactarMuitos(IEnumerable<byte[]> arquivosEmBytes, string caminhoDeSaidaZip)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var zip = new ZipArchive(memoryStream, ZipArchiveMode.Create))
+                    foreach(var bytes in arquivosEmBytes)
+                        using (var streamDeEscrita = new BinaryWriter(zip.CreateEntry(caminhoDeSaidaZip).Open()))
+                            streamDeEscrita.Write(bytes);
+
+                return memoryStream.ToArray();
+            }
+        }
+    }
+}
