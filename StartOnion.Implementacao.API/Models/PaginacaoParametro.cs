a08158ea6eb8e72075b627e8762324263eb4ae47
Élix de Paula Rezende Neto <elixneto@gmail.com>
2020-02-02 01:19:22 -0400

    Paginação API

---
+++ StartOnion.Implementacao.API/Models/PaginacaoParametro.cs
@@ -0,0 +1,19 @@
+﻿namespace StartOnion.Implementacao.API.Models
+{
+    /// <summary>
+    /// Paginação
+    /// </summary>
+    public sealed class PaginacaoParametro
+    {
+        private uint _quantidadePorPagina = 100;
+
+        /// <summary>
+        /// Número da página
+        /// </summary>
+        public uint Pagina { get; set; }
+        /// <summary>
+        /// Quantidade de registros por página (máximo 100)
+        /// </summary>
+        public uint QuantidadePorPagina { get => _quantidadePorPagina; set { if (value > 100) _quantidadePorPagina = 100; } }
+    }
+}
