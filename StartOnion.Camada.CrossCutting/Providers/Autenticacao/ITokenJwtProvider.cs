5f0790a3de13becd6461c37eb03439146b925018
Élix Neto <elixneto@gmail.com>
2020-02-05 12:51:54 -0400

    Gerando tokens com multiplas opções ToenJwtProvider

--- StartOnion.Camada.CrossCutting/Providers/Autenticacao/ITokenJwtProvider.cs
+++ StartOnion.Camada.CrossCutting/Providers/Autenticacao/ITokenJwtProvider.cs
@@ -13,10 +13,32 @@ namespace StartOnion.Camada.CrossCutting.Providers.Autenticacao
         /// Retorna o token JWT
         /// </summary>
         /// <param name="Id">Identificador único do usuário</param>
+        /// <returns></returns>
+        string GerarToken(string Id);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
+        /// <param name="dataDeExpiracao">Data de expiração do token</param>
+        /// <returns></returns>
+        string GerarToken(string Id, DateTime? dataDeExpiracao);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
         /// <param name="roles">Lista de roles a serem incluídas como claims</param>
         /// /// <param name="dataDeExpiracao">Data de expiração do token</param>
         /// <returns></returns>
-        string GerarToken(string Id, IEnumerable<string> roles, DateTime dataDeExpiracao);
+        string GerarToken(string Id, IEnumerable<string> roles, DateTime? dataDeExpiracao);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
+        /// <param name="customClaims">Claims personalizados</param>
+        /// <param name="roles">Lista de roles a serem incluídas como claims</param>
+        /// <param name="dataDeExpiracao">Data de expiração do token</param>
+        /// <returns></returns>
+        string GerarToken(string Id, IDictionary<string, string> claimsPersonalizados, IEnumerable<string> roles, DateTime? dataDeExpiracao);
         /// <summary>
         /// Issuer do JWT
         /// </summary>
--- StartOnion.Camada.CrossCutting/Providers/Autenticacao/TokenJwtProvider.cs
+++ StartOnion.Camada.CrossCutting/Providers/Autenticacao/TokenJwtProvider.cs
@@ -38,15 +38,46 @@ namespace StartOnion.Camada.CrossCutting.Providers.Autenticacao
         /// Retorna o token JWT
         /// </summary>
         /// <param name="Id">Identificador único do usuário</param>
+        /// <returns></returns>
+        public string GerarToken(string Id)
+            => GerarTokenJwt(Id, null, null, null);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
+        /// <param name="dataDeExpiracao">Data de expiração do token</param>
+        /// <returns></returns>
+        public string GerarToken(string Id, DateTime? dataDeExpiracao)
+            => GerarTokenJwt(Id, null, null, dataDeExpiracao);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
         /// <param name="roles">Lista de roles a serem incluídas como claims</param>
         /// /// <param name="dataDeExpiracao">Data de expiração do token</param>
         /// <returns></returns>
-        public string GerarToken(string Id, IEnumerable<string> roles, DateTime dataDeExpiracao)
+        public string GerarToken(string Id, IEnumerable<string> roles, DateTime? dataDeExpiracao)
+            => GerarTokenJwt(Id, null, roles, dataDeExpiracao);
+        /// <summary>
+        /// Retorna o token JWT
+        /// </summary>
+        /// <param name="Id">Identificador único do usuário</param>
+        /// <param name="customClaims">Claims personalizados</param>
+        /// <param name="roles">Lista de roles a serem incluídas como claims</param>
+        /// <param name="dataDeExpiracao">Data de expiração do token</param>
+        /// <returns></returns>
+        public string GerarToken(string Id, IDictionary<string, string> claimsPersonalizados, IEnumerable<string> roles, DateTime? dataDeExpiracao)
+            => GerarTokenJwt(Id, claimsPersonalizados, roles, dataDeExpiracao);
+        
+
+        private string GerarTokenJwt(string Id, IDictionary<string, string> customClaims, IEnumerable<string> roles, DateTime? dataDeExpiracao)
         {
             var _claimOptions = new IdentityOptions();
             var claims = new List<Claim>();
             claims.Add(new Claim("sub", Id));
-            //if (roles != null)
+            if (customClaims != null)
+                claims.AddRange(customClaims.Select(c => new Claim(c.Key, c.Value)));
+            if (roles != null)
                 claims.AddRange(roles.Select(r => new Claim(ClaimTypes.Role, r)));
 
             return new JwtSecurityTokenHandler()
