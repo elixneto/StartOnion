421d248028b0eb80cfa942321392948b1cee40b1
Élix Neto <elixneto@gmail.com>
2020-02-14 14:47:24 -0400

    v2.2.2.1 :: atualizando o Id com parametro no consturtor da Entidade

--- StartOnion.Camadas.Testes.Unidade/Dominio/EntidadeTeste.cs
+++ StartOnion.Camadas.Testes.Unidade/Dominio/EntidadeTeste.cs
@@ -8,7 +8,7 @@ namespace StartOnion.Camadas.Testes.Unidade.Dominio
 {
     public class EntidadeTeste
     {
-        class MinhaEntidade : Entidade { public MinhaEntidade() : base() { } public MinhaEntidade(IValidator validador) : base(validador) { } }
+        class MinhaEntidade : Entidade { public MinhaEntidade() : base() { } public MinhaEntidade(Guid id) : base(id) { } public MinhaEntidade(IValidator validador) : base(validador) { } }
 
         [Fact]
         public void DeveGerarIdComoGuid()
@@ -34,6 +34,16 @@ namespace StartOnion.Camadas.Testes.Unidade.Dominio
             var minhaEntidade = new MinhaEntidade();
 
             Assert.False(minhaEntidade.Id == default);
+        }
+
+        [Fact]
+        public void DeveAlterarOId()
+        {
+            var guidEsperado = new Guid("ba9c480a-6370-415e-adf7-20cb715ba9b6");
+
+            var minhaEntidade = new MinhaEntidade(guidEsperado);
+
+            Assert.Equal(guidEsperado, minhaEntidade.Id);
         }
     }
 }
