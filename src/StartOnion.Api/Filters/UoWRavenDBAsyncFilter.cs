69a129aa36f9e655a46e9241026ffc210291a547
Élix de Paula Rezende Neto <elixneto@gmail.com>
2020-05-23 18:39:04 -0400

    api filter for async repository :: RavenDB

---
+++ src/StartOnion.Api/Filters/UoWRavenDBAsyncFilter.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using StartOnion.CrossCutting.Notifications;
+using StartOnion.Repository.RavenDB.Contexts;
+
+namespace StartOnion.Api.Filters
+{
+    public class UoWRavenDBAsyncFilter : IActionFilter
+    {
+        private readonly INotificationContext _notificator;
+        private readonly ContextRepositoryRavenDBAsync _context;
+
+        public UoWRavenDBAsyncFilter(INotificationContext notificator, ContextRepositoryRavenDBAsync context)
+        {
+            _notificator = notificator;
+            _context = context;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            
+        }
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (!_notificator.HasNotifications())
+                _context.Commit();
+            else
+                _context.Rollback();
+        }
+    }
+}
