ca42ac01eacb971cdb601b5f8b6d230bb0ebf46f
Élix Neto <elixneto@gmail.com.br>
2021-01-15 18:50:30 -0400

    collection and list :: readonly

--- src/StartOnion.CrossCutting/Extensions/Linq/EnumerableExtensions.cs
+++ src/StartOnion.CrossCutting/Extensions/Linq/EnumerableExtensions.cs
@@ -14,6 +14,14 @@ namespace StartOnion.CrossCutting.Extensions.Linq
         /// <typeparam name="T"></typeparam>
         /// <param name="enumerable"></param>
         /// <returns></returns>
-        public static IReadOnlyCollection<T> AsReadOnly<T>(this IEnumerable<T> enumerable) => enumerable.ToList().AsReadOnly();
+        public static IReadOnlyCollection<T> AsCollectionReadOnly<T>(this IEnumerable<T> enumerable) => enumerable.ToList().AsReadOnly();
+
+        /// <summary>
+        /// ToList().AsReadOnly()
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="enumerable"></param>
+        /// <returns></returns>
+        public static IReadOnlyList<T> AsListReadOnly<T>(this IEnumerable<T> enumerable) => enumerable.ToList().AsReadOnly();
     }
 }
