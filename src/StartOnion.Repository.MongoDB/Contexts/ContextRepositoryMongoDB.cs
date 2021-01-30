4c075e825d7c195041d6dbebfbc80b3b3759da6d
Élix Neto <elixneto@gmail.com.br>
2021-01-30 16:27:23 -0400

    not async MongoDB

---
+++ src/StartOnion.Repository.MongoDB/Contexts/ContextRepositoryMongoDB.cs
@@ -0,0 +1,50 @@
+﻿using MongoDB.Driver;
+using StartOnion.Repository.MongoDB.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace StartOnion.Repository.MongoDB.Contexts
+{
+    public class ContextRepositoryMongoDB
+    {
+        public IMongoClient Client { get; private set; }
+        public IMongoDatabase Database { get; set; }
+        public IClientSession Session { get; private set; }
+
+        private readonly List<Func<Task>> _commands;
+
+        public ContextRepositoryMongoDB(MongoDBConfiguration configuration)
+        {
+            Client = configuration.Client;
+            Database = configuration.Database;
+            _commands = new List<Func<Task>>();
+        }
+
+        public void AddCommand(Func<Task> command) => _commands.Add(command);
+
+        public async void Commit()
+        {
+            using (Session = Client.StartSession())
+            {
+                Session.StartTransaction();
+
+                var commandTasks = _commands.Select(c => c());
+
+                await Task.WhenAll(commandTasks);
+
+                await Session.CommitTransactionAsync();
+            }
+        }
+
+        public void Rollback()
+        {
+            while (Session != null && Session.IsInTransaction)
+                Thread.Sleep(TimeSpan.FromMilliseconds(100));
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ src/StartOnion.Repository.MongoDB/RepositoryMongoDB.cs
@@ -0,0 +1,52 @@
+﻿using MongoDB.Driver;
+using StartOnion.Domain;
+using StartOnion.Domain.Interfaces;
+using StartOnion.Repository.MongoDB.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StartOnion.Repository.MongoDB
+{
+    public abstract class RepositoryMongoDB<TEntity> : IRepository<TEntity> where TEntity : Entity
+    {
+        public readonly ContextRepositoryMongoDB ContextMongoDB;
+        public readonly IMongoCollection<TEntity> DbSet;
+        public readonly FilterDefinitionBuilder<TEntity> Filter;
+
+        protected RepositoryMongoDB(ContextRepositoryMongoDB context, string collectionName)
+        {
+            ContextMongoDB = context;
+            DbSet = ContextMongoDB.Database.GetCollection<TEntity>(collectionName);
+            Filter = Builders<TEntity>.Filter;
+        }
+
+        public void Add(TEntity entity) => ContextMongoDB.AddCommand(() => DbSet.InsertOneAsync(entity));
+
+        public void Add(ICollection<TEntity> entities) => ContextMongoDB.AddCommand(async () => await DbSet.InsertManyAsync(entities));
+
+        public IEnumerable<TEntity> GetAll() => DbSet.Find(DefaultFilters<TEntity>.Empty()).ToList();
+
+        public TEntity GetById(Guid id) => DbSet.Find(DefaultFilters<TEntity>.EqualsById(id)).SingleOrDefault();
+
+        public TEntity GetById(string id) => DbSet.Find(DefaultFilters<TEntity>.EqualsById(id)).SingleOrDefault();
+
+        public void Remove(TEntity entity) =>
+            ContextMongoDB.AddCommand(() => DbSet.DeleteOneAsync(DefaultFilters<TEntity>.EqualsById(entity)));
+
+        public void Remove(ICollection<TEntity> entities)
+        {
+            foreach (var entity in entities)
+                Remove(entity);
+        }
+
+        public void Update(TEntity entity) => 
+            ContextMongoDB.AddCommand(() => DbSet.ReplaceOneAsync(DefaultFilters<TEntity>.EqualsById(entity.Id), entity));
+
+        public void Update(ICollection<TEntity> entities)
+        {
+            foreach (var entity in entities)
+                Update(entity);
+        }
+    }
+}
